43ffa3b891234e302798108c80ddb1d1df459043
deffsin <deffsinco@gmail.com>
2024-06-05 18:20:01 +0300

    Initial Commit

---
+++ ExerciseWallester/ExerciseWallesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseWallesterApp.swift
+//  ExerciseWallester
+//
+//  Created by Denis Sinitsa on 05.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseWallesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
